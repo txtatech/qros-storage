5eeb19a291cef7bad3598659008b6b35e1f634b9
txtatech <96074703+txtatech@users.noreply.github.com>
2023-11-09 02:16:34 -0500

    Add files via upload

---
+++ qros-storage/qros-dna-main/qros-dna-start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Start
+python3 qros_dna_main.py
+
+# Wait
+wait
+
+echo Working... Chunks Created
+
+# Start
+python3 qros_dna_squashfs.py
+
+# Wait
+wait
+
+echo Working... Created Squashfs
+
+# Wait
+wait
+
+# Start
+python3 qros_dna_chunksquash.py
+
+# Wait
+wait
+
+echo Working... Chunked Squashfs 
+
+# Start
+python3 qros_dna_os.py
+
+# Wait
+wait
+
+echo Working... Created Live DNA OS
+
+# Start
+python3 qros_dna_os_fs.py
+
+# Wait
+wait
+
+echo Working... Created Live DNA OS FS
+
+echo Encoding has finished and all files have been generated!
+
+# Wait
+wait
